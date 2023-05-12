96df0225a8461e23a5be929f6407f4ad27990a8c
Maciej Berka <maciejberka@gmail.com>
2023-05-12 14:10:38 +0200

    no need to watch notifier

--- lib/screens/player_screen.dart
+++ lib/screens/player_screen.dart
@@ -10,7 +10,7 @@ class PlayerScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final provider = ref.watch(playerProvider.notifier);
+    final provider = ref.read(playerProvider.notifier);
     final state = ref.watch(playerProvider);
     return APScaffold(
       context: context,
